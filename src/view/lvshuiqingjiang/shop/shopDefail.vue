609e1950ae69f32c4de26b3e00d04b6651414af3
dongfang <1163129165@qq.com>
2018-11-19 17:52:18 +0800

    commit

--- src/view/lvshuiqingjiang/shop/shopDefail.vue
+++ src/view/lvshuiqingjiang/shop/shopDefail.vue
@@ -1,15 +1,147 @@
 <template>
     <div>
-
+        <Spin size="large" fix v-if="spinShow"></Spin>
+        <row>
+            <i-col style="margin-bottom:20px;">
+                <Page :total="total" :page-size="pre_page" :on-change='changePage' />
+            </i-col>
+            <i-col>
+                <i-table size="large" style="min-width:800px;" :columns="goodsColunm" :data="goodsList"></i-table>
+            </i-col>
+        </row>
     </div>
 </template>
 
 <script>
-    export default {
-        
+import axios from "@/libs/api.request";
+import Cookies from "js-cookie";
+export default {
+    data() {
+        return {
+            currentPage:1,
+            total:1,
+            pre_page:1,
+            spinShow: false,
+            currentId:'',
+            typeList: [],
+            goodsType:[],
+            goodslist:[],
+            goodsColunm:[
+                {
+                    title:'商品名',
+                    key:'name'
+                },{
+                    title:'商品类型',
+                    key:'type'
+                },{
+                    title:'总数',
+                    key:'total'
+                },{
+                    title:'价格',
+                    key:'price'
+                },{
+                    title:'优惠价',
+                    key:'discount'
+                },{
+                    title:'月销',
+                    key:'monthly_sales'
+                },{
+                    title:'上架',
+                    key:'is_up'
+                },{
+                    title:'操作',
+                    render: (h, params) => {
+                        return h("div", [
+                            h(
+                                "Button",
+                                {
+                                    props: {
+                                        type: "primary",
+                                        size: "small"
+                                    },
+                                    attrs: {
+                                        style: "font-size:12px"
+                                    },
+                                    nativeOn: {
+                                        click: () => {
+                                            this.currentId = params.row.id;
+                                            this.dataTitle = "修改";
+                                            this.isNew = false
+                                        }
+                                    }
+                                },
+                                "编辑"
+                            ),
+                            h(
+                                "Button",
+                                {
+                                    props: {
+                                        type: "error",
+                                        size: "small"
+                                    },
+                                    attrs: {
+                                        style:
+                                            "font-size:12px;margin-left:10px;"
+                                    },
+                                    nativeOn: {
+                                        click: () => {
+                                            this.currentId = params.row.id;
+                                        }
+                                    }
+                                },
+                                "删除"
+                            )
+                        ]);
+                    }
+                }
+            ]
+        };
+    },
+    methods: {
+        changePage(index){
+            this.currentPage = index
+            this.getGoodLost()
+        },
+        getSwipeType(){
+            axios
+                .request({
+                    url: "mall-parameter/",
+                    method: "get"
+                })
+                .then(res => {
+                    this.goodsType = res.data.swiper
+                });
+        },
+        getType() {
+            axios
+                .request({
+                    url: "mall-navs",
+                    method: "get"
+                })
+                .then(res => {
+                    this.typeList = res.data;
+                });
+        },
+        getGoodLost(){
+            this.spinShow = true;
+            axios
+                .request({
+                    url: "mall-goods?page="+this.currentPage,
+                    method: "get"
+                })
+                .then(res => {
+                    this.goodsList = res.data.data;
+                    
+                    this.spinShow = false;
+                });
+        }
+    },
+    mounted() {
+        this.getGoodLost()
+        this.getType()
     }
+};
 </script>
 
 <style scoped>
-
 </style>
--- src/view/lvshuiqingjiang/shop/shopSwipe.vue
+++ src/view/lvshuiqingjiang/shop/shopSwipe.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <row>
+        <row style="min-width:1300px;">
             <i-col style="width:640px;height:480px; float:left;margin-bottom:20px;margin-right:20px;">
                 <Carousel v-model="value" loop>
                     <CarouselItem v-for="item in defaultList">
@@ -16,15 +16,7 @@
         <Modal v-model="newGroundReview" :title="reviewTitle" @on-ok="newLunboGround" @on-cancel="openNewGround(false)">
             <row>
                 <i-col>
-                    轮播组名字
-                </i-col>
-                <i-col>
-                    <Input v-model="newGroudName" style="width: 300px" />
-                </i-col>
-            </row>
-            <row>
-                <i-col>
-                    轮播组显示
+                    swiperGround名字
                 </i-col>
                 <i-col>
                     <Input v-model="newGroudName" style="width: 300px" />
@@ -34,47 +26,57 @@
         <Modal v-model="groundPic" title="编辑轮播组图片" @on-ok="" @on-cancel="openGroundPic(false)">
             <row>
                 <i-col>
-                    <Upload action="https://www.rdoorweb.com/pzhan/public/qiniu/upload" :on-success='successUpload' :on-preview='previewUpload' :show-upload-list='false' :default-file-list="defaultList" :headers="headers">
+                    <Upload action="http://localhost:8000/qiniu/upload" :on-success='successUpload' :on-preview='previewUpload' :show-upload-list='false' :default-file-list="defaultList" :headers="headers">
                         <Button icon="ios-cloud-upload-outline">上传图片</Button>
                     </Upload>
                 </i-col>
-                <!-- <i-col>
+                <i-col>
                     <div style="width:100%;height:600px;margin-top:10px;border:1px solid #ddd;overflow-y:scroll;overflow-x:hidden;">
-                        <div v-for='(item,index) in defaultList' :key="index" @click="previewUpload(index)" style="border:1px solid #eee;width:224px;height:224px;float:left;margin:10px 5px 0;overflow:hidden;position:relative;">
+                        <div v-for='(item,index) in currentList' :key="index" @click="previewUpload(index)" style="border:1px solid #eee;width:224px;height:224px;float:left;margin:10px 5px 0;overflow:hidden;position:relative;">
                             <span @click.stop="openDeletePic(true,index)" style="position:absolute;margin-top:0;margin:left;background:#aaa;color:#fff;display:block;width:20px;height:20px;line-height:20px;text-align:center;cursor: pointer">
                                 ╳
                             </span>
                             <img :src="item.image" height="100%" style="display:block;margin:0 auto;">
                         </div>
                     </div>
-                </i-col> -->
+                </i-col>
             </row>
             <div slot="footer">
             </div>
         </Modal>
         <Modal v-model="picData" title="图片信息" @on-ok="inputData()" @on-cancel="openPicData(false)">
-            <row>
+            <row style="margin-top:10px;">
                 <i-col>
-                    跳转链接
+                    跳转类型
                 </i-col>
                 <i-col>
-                    <Input v-model="previewData.url"/>
+                    <RadioGroup v-model="previewData.type" type="button">
+                        <Radio :label="item.value" v-for="item in swipeType">{{item.ch}}</Radio>
+                    </RadioGroup>
                 </i-col>
             </row>
             <row style="margin-top:10px;">
                 <i-col>
+                    跳转链接
+                </i-col>
+                <i-col>
+                    <Input v-model="previewData.url" />
+                </i-col>
+            </row>
+            <!-- <row style="margin-top:10px;">
+                <i-col>
                     图片名称
                 </i-col>
                 <i-col>
-                    <Input v-model="previewData.name"/>
+                    <Input v-model="previewData.name" />
                 </i-col>
-            </row>
+            </row> -->
             <row style="margin-top:10px;">
                 <i-col>
                     备注
                 </i-col>
                 <i-col>
-                    <Input v-model="previewData.remake"/>
+                    <Input v-model="previewData.remake" />
                 </i-col>
             </row>
             <row style="margin-top:10px;">
@@ -94,6 +96,14 @@
                 </i-col>
             </row>
         </Modal>
+        <Modal v-model="groupModal" title="删除" @on-ok="deleteGroup()" @on-cancel="openDelete(false)">
+            <row>
+                <i-col style="margin:0 auto;">
+                    是否删除轮播组
+                    --- <span style="color:red;">{{currentName}}</span> ----
+                </i-col>
+            </row>
+        </Modal>
     </div>
 </template>
 
@@ -103,6 +113,7 @@ import Cookies from "js-cookie";
 export default {
     data() {
         return {
+            groupModal: false,
             value: 0,
             receivedColumn: [
                 {
@@ -120,7 +131,7 @@ export default {
                                 },
                                 nativeOn: {
                                     click: () => {
-                                        this.changeDisplay(params.row.id);
+                                        this.changeDisplay(params.row.id,params.row.display);
                                     }
                                 }
                             },
@@ -145,9 +156,9 @@ export default {
                                     },
                                     nativeOn: {
                                         click: () => {
-                                            // this.getGroupPic(params.row.id);
+                                            this.getGroupPic(params.row.id);
                                             this.groupId = params.row.id;
-                                            this.openNewGround(true);
+                                            this.openGroundPic(true);
                                         }
                                     }
                                 },
@@ -162,8 +173,8 @@ export default {
                                     },
                                     nativeOn: {
                                         click: () => {
-                                            this.removeId = params.row.id;
-                                            this.removeName = params.row.name;
+                                            this.currentId = params.row.id;
+                                            this.currentName = params.row.name;
                                             this.openDelete(true);
                                         }
                                     }
@@ -174,26 +185,24 @@ export default {
                     }
                 }
             ],
-            displayDataId:'',
-
+            displayDataId: "",
             receivedData: [],
             reviewTitle: "新增组",
             lunboList: [],
-
             groundIsNew: true,
             newGroundReview: false,
             newGroudName: "",
-            groupData:{
-
-            },
             groupId: "",
             groundPic: false,
-            defaultList: [{},{}],
-            previewData:'',
-            picData:false,
-
-            deleteModal:false,
-            deleteId:''
+            defaultList: [{}],
+            currentList: [],
+            previewData: "",
+            picData: false,
+            deleteModal: false,
+            deleteId: "",
+            currentId: "",
+            currentName: "",
+            swipeType:[]
         };
     },
     computed: {
@@ -204,6 +213,31 @@ export default {
         }
     },
     methods: {
+        //getSwipeType
+        getSwipeType(){
+            axios
+                .request({
+                    url: "mall-parameter/",
+                    method: "get"
+                })
+                .then(res => {
+                    this.swipeType = res.swiper
+                });
+        },
+        openDelete(i) {
+            this.groupModal = i;
+        },
+        deleteGroup() {
+            axios
+                .request({
+                    url: "mall-groups/" + this.currentId,
+                    method: "delete"
+                })
+                .then(res => {
+                    this.$Message.success("删除成功");
+                    this.getLunboGround();
+                });
+        },
         getLunboGround() {
             axios
                 .request({
@@ -212,9 +246,21 @@ export default {
                 })
                 .then(res => {
                     this.receivedData = res.data;
-                    for(let i=0;i<this.receivedData.length;i++){
-                        if(this.receivedData[i].display === 1){
-                            this.getGroupPic(this.receivedData[i].id)
+                    for (let i = 0; i < this.receivedData.length; i++) {
+                        if (this.receivedData[i].display === 1) {
+                            axios
+                                .request({
+                                    url: "mall-groups/" + this.receivedData[i].id,
+                                    method: "get"
+                                })
+                                .then(res => {
+                                    console.log(res);
+                                    
+                                    this.defaultList = res.data[0].swipers;
+                                    if (this.defaultList === null) {
+                                        this.defaultList = [{},{}];
+                                    }
+                                });
                         }
                     }
                 });
@@ -225,19 +271,16 @@ export default {
         openGroundPic(i) {
             this.groundPic = i;
         },
-        openPicData(i){
-            this.openGroundPic(!i)
+        openPicData(i) {
+            this.openGroundPic(!i);
             this.picData = i;
         },
-        openDeletePic(i,index){
-            this.deleteModal = i
-            if(i){
-                this.deleteId = this.defaultList[index].id
+        openDeletePic(i, index) {
+            this.deleteModal = i;
+            if (i) {
+                this.deleteId = this.currentList[index].id;
             }
         },
-
-
-
         //getGroupPic
         getGroupPic(id) {
             axios
@@ -246,10 +289,14 @@ export default {
                     method: "get"
                 })
                 .then(res => {
-                    this.defaultList = res.data.swiper
+                    console.log(res.data[0]);
+                    
+                    this.currentList = res.data[0].swipers;
+                    if (this.currentList === null) {
+                        this.currentList = [{},{}];
+                    }
                 });
         },
-
         //newLunboGround
         newLunboGround() {
             if (this.newGroudName === "") {
@@ -258,7 +305,7 @@ export default {
             }
             axios
                 .request({
-                    url: "swiper_groups",
+                    url: "mall-groups",
                     method: "post",
                     data: {
                         display: 0,
@@ -271,14 +318,13 @@ export default {
                 });
         },
         ///changeDisplay
-        changeDisplay(id) {
+        changeDisplay(id,is) {
             axios
                 .request({
-                    url: "swiper_groups/" + id,
+                    url: "mall-groups/" + id + "/change",
                     method: "put",
                     data:{
-                        name:'',
-                        display:''
+                        is_up:is
                     }
                 })
                 .then(res => {
@@ -286,12 +332,10 @@ export default {
                     this.getLunboGround();
                 });
         },
-
         //successUpload
         successUpload(file) {
             this.inputGroup(file);
         },
-
         //提交groupPic
         // data
         // msg: "上传成功"
@@ -300,15 +344,16 @@ export default {
         inputGroup(data) {
             axios
                 .request({
-                    url: "swipers",
+                    url: "mall-swipers",
                     method: "post",
                     data: {
+                        type:'other',
+                        url: "www.123.com",
                         display: 1,
+                        url_id:'',
                         group: this.groupId,
-                        name: "未命名",
                         image: data.url,
-                        remake: "未备注",
-                        url: "www.rdoorweb.com"
+                        remake: "未备注"
                     }
                 })
                 .then(res => {
@@ -316,34 +361,31 @@ export default {
                     this.getGroupPic(this.groupId);
                 });
         },
-
         //clickPic
-        previewUpload(index){
-            this.previewData = this.defaultList[index]
-            this.openPicData(true)
-            console.log(index);
+        previewUpload(index) {
+            console.log(this.currentList[index]);
             
+            this.previewData = this.currentList[index];
+            this.openPicData(true);
         },
-        inputData(){
+        inputData() {
             axios
                 .request({
-                    url: "swipers/"+this.previewData.id,
+                    url: "mall-swipers/" + this.previewData.id,
                     method: "put",
                     data: this.previewData
                 })
                 .then(res => {
                     this.$Message.success("success");
-                    this.openGroundPic(true)
+                    this.openGroundPic(true);
                     this.getGroupPic(this.groupId);
                 });
         },
-
-
         // deletePic
-        deletePic(index){
-                axios
+        deletePic(index) {
+            axios
                 .request({
-                    url: "swipers/"+this.deleteId,
+                    url: "mall-swipers/" + this.deleteId,
                     method: "delete"
                 })
                 .then(res => {
@@ -354,6 +396,7 @@ export default {
     },
     mounted() {
         this.getLunboGround();
+        this.getSwipeType()
     }
 };
 </script>
